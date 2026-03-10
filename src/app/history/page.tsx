3c38d32d5915b7577304aafca7bcf642472f462c
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-10 20:51:39 -0300

    Create page.tsx

---
+++ src/app/history/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import BottomNav from "@/components/BottomNav"
+
+export default function History(){
+
+return(
+
+<div className="min-h-screen bg-slate-950 text-white p-6 pb-24">
+
+<h1 className="text-3xl font-bold mb-6">
+History
+</h1>
+
+<div className="bg-slate-900 p-6 rounded-xl border border-slate-700">
+
+<p className="text-slate-400">
+Histórico de sinais aparecerá aqui.
+</p>
+
+</div>
+
+<BottomNav/>
+
+</div>
+
+)
+
+}
