d770903c2f94f0dc76863e1944ef5fd0c4702d97
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-10 22:26:30 -0300

    Update route.ts

--- src/app/api/price/[symbol]/route.ts
+++ src/app/api/price/[symbol]/route.ts
@@ -5,33 +5,33 @@ req: Request,
 { params }: { params: { symbol: string } }
 ){
 
-const map:any = {
-BTCUSD:"BTCUSDT",
-XAUUSD:"BTCUSDT",
-EURUSD:"ETHUSDT",
-GBPUSD:"BNBUSDT",
-USDJPY:"SOLUSDT",
-EURJPY:"ADAUSDT"
-}
-
-const symbol = map[params.symbol] || "BTCUSDT"
-
 try{
 
-const res = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`)
+const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,cardano,binancecoin&vs_currencies=usd")
 
 const data = await res.json()
 
+const map:any = {
+
+BTCUSD:data.bitcoin.usd,
+XAUUSD:data.ethereum.usd,
+EURUSD:data.solana.usd,
+GBPUSD:data.binancecoin.usd,
+USDJPY:data.cardano.usd,
+EURJPY:data.ethereum.usd
+
+}
+
 return NextResponse.json({
 symbol: params.symbol,
-price: Number(data.price)
+price: map[params.symbol] || 0
 })
 
 }catch{
 
 return NextResponse.json({
 symbol: params.symbol,
-price: 0
+price:0
 })
 
 }
