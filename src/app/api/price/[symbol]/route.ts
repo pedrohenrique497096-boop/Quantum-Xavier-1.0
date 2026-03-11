6656d45d7085ef3d12c0992680880684c34b4cf8
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-10 23:52:58 -0300

    Update route.ts

--- src/app/api/price/[symbol]/route.ts
+++ src/app/api/price/[symbol]/route.ts
@@ -2,42 +2,45 @@ import { NextResponse } from "next/server"
 
 const API_KEY = "1bbb904ae0994fb7b2d120da18c66602"
 
-export async function GET(
-req: Request,
-{ params }: { params: { symbol: string } }
-){
+const symbols = [
+"BTCUSD",
+"XAUUSD",
+"EURUSD",
+"GBPUSD",
+"USDJPY",
+"EURJPY"
+]
 
-const map:any = {
-BTCUSD:"BTC/USD",
-XAUUSD:"XAU/USD",
-EURUSD:"EUR/USD",
-GBPUSD:"GBP/USD",
-USDJPY:"USD/JPY",
-EURJPY:"EUR/JPY"
-}
+export async function GET(){
 
-const symbol = map[params.symbol]
+const prices = []
+
+for(const symbol of symbols){
 
 try{
 
 const res = await fetch(
-`https://api.twelvedata.com/price?symbol=${symbol}&apikey=${API_KEY}`
+`http://api.marketstack.com/v1/tickers/${symbol}?access_key=${API_KEY}`
 )
 
 const data = await res.json()
 
-return NextResponse.json({
-symbol: params.symbol,
-price: Number(data.price)
+prices.push({
+symbol,
+price: data?.last || null
 })
 
 }catch{
 
-return NextResponse.json({
-symbol: params.symbol,
-price:0
+prices.push({
+symbol,
+price:null
 })
 
 }
+
+}
+
+return NextResponse.json(prices)
 
 }
