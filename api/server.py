f591430a1b5ad6eab16329708b418d6d9e40aff6
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-11 00:03:13 -0300

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from ai.scanner import generate_signals
+from ia.scanner import signals, start_scanner
+import threading
 
 app = FastAPI()
 
 @app.get("/signals")
 def get_signals():
-    return generate_signals()
+    return signals
+
+
+def run_scanner():
+    start_scanner()
+
+
+thread = threading.Thread(target=run_scanner)
+thread.start()
