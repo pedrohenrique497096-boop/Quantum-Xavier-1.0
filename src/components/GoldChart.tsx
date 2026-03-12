b25085f1b4450833fa9819992cbd638f418d1ee2
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-12 05:43:41 -0300

    Update GoldChart.tsx

--- src/components/GoldChart.tsx
+++ src/components/GoldChart.tsx
@@ -1,65 +1,18 @@
 "use client"
 
-import { useEffect, useRef } from "react"
-
 export default function GoldChart(){
-
-const container = useRef<HTMLDivElement>(null)
-
-useEffect(()=>{
-
-if(!container.current) return
-
-container.current.innerHTML = ""
-
-const script = document.createElement("script")
-script.src = "https://s3.tradingview.com/tv.js"
-script.async = true
-
-script.onload = () => {
-
-new (window as any).TradingView.widget({
-
-autosize:true,
-
-symbol:"OANDA:XAUUSD",
-
-interval:"5",
-
-timezone:"Etc/UTC",
-
-theme:"dark",
-
-style:"1",
-
-locale:"en",
-
-toolbar_bg:"#020617",
-
-enable_publishing:false,
-
-hide_top_toolbar:false,
-
-save_image:false,
-
-container_id:"gold_chart"
-
-})
-
-}
-
-container.current.appendChild(script)
-
-},[])
 
 return(
 
-<div className="w-full">
+<div className="w-full h-[500px] rounded-xl overflow-hidden">
 
-<div id="gold_chart" className="h-[420px] w-full"/>
+<iframe
+src="https://s.tradingview.com/widgetembed/?symbol=OANDA:XAUUSD&interval=5&hidesidetoolbar=1&theme=dark"
+className="w-full h-full border-0"
+/>
 
 </div>
 
 )
 
-  }
+}
